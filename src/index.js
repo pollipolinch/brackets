d19f4bdf538c75e9029ee83809b8b38d60e2c0be
Полина <a1@MacBook-Air-Polina.local>
2023-02-10 01:59:34 +0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false
+  }
+  let newStr = str.split('')
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (newStr === bracketsConfig[i].join('')) {
+      return true
+    }
+
+    for (let k = 1; k <= newStr.length; k++) {
+      if (
+        newStr[k - 1] === bracketsConfig[i].join('')[0] && newStr[k] === bracketsConfig[i].join('')[1]) {
+          newStr.splice(k - 1, 2)
+        k = 0
+      }
+    }
+
+    if (newStr.length === 0) {
+      return true
+    }
+  }
+  let result =''
+  if (str.length > newStr.join('').length) {
+    str = newStr.join('')
+    result = check(str, bracketsConfig)
+    return result
+  }
+  return false
 }
